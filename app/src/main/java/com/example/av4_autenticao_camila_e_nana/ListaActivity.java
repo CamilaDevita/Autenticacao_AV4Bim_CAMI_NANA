f017611f28c6a8a35c8800cb9d6292236a2845a9
camila <c.devita@aluno.ifsp.edu.br>
2022-12-08 19:02:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/av4_autenticao_camila_e_nana/ListaActivity.java
@@ -0,0 +1,51 @@
+package com.example.av4_autenticao_camila_e_nana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ListaActivity extends AppCompatActivity {
+
+    // Atributos dos objetos da GUI:
+    private TextView lblUsuario;
+    private EditText txtNome;
+    private EditText txtNota1;
+    private EditText txtNota2;
+    private Button btnInserir;
+    private Button btnSair;
+    private ListView lista;
+    // Atributo que é a referência para o nó raiz do BD no Firebase:
+    private DatabaseReference BD = FirebaseDatabase.getInstance().getReference();
+    // Atributo que é referência para o gerenciador de usuários:
+    private FirebaseAuth usuarios = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+
+    @Override
+    public void onBackPressed() {
+// Vazio pra desabilitar o back
+    }
+    private class EscutadorSair implements View.OnClickListener {
+
+    }
+    private class EscutadorInserir implements View.OnClickListener {
+
+    }
+    private class AlunosAdapter extends FirebaseListAdapter<Aluno> {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/av4_autenticao_camila_e_nana/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.av4_autenticao_camila_e_nana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Atributos dos objetos da GUI:
+    private EditText txtEmail;
+    private EditText txtSenha;
+    private Button btnLogin;
+    private Button btnCriar;
+
+    // Atributo que é referência para o gerenciador de usuários:
+    private FirebaseAuth usuarios = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCriar = findViewById(R.id.btnCriar);
+
+    }
+
+    private class EscutadorLogin implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+    private class EscutadorCriar implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+}
